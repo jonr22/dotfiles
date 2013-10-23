82021eb9cafe1eb1854589f6fd49ef1fed7bd449
jonr22 <jonr2219@gmail.com>
2013-10-22 21:39:54 -0400

    added osx plugin to zsh

---
+++ tmux/standard.zsh
@@ -0,0 +1,22 @@
+#!/bin/zsh
+
+if [ -n "$1" ]; then
+  SESSION=$1
+else
+  SESSION=$USER
+fi
+
+tmux new-session -s $SESSION -d
+
+tmux rename-window main
+
+tmux split-window -v -p 25
+tmux split-window -h
+
+tmux select-pane -t 0
+tmux split-window -h -p 25 -d
+
+tmux new-window -n scratch
+tmux split-window -v
+tmux select-window -t 1
+
