1a6359cac8a45a8ab42246d08a5fcbced97eca1e
jonr22 <jonr2219@gmail.com>
2014-07-28 22:02:25 -0400

    improve tmux scripts

--- scripts/tmux/terminal.zsh
+++ scripts/tmux/terminal.zsh
@@ -1,53 +1,54 @@
 #!/bin/zsh
 
-### Create Session ###
-#
-if [ -n "$1" ]; then
-  SESSION=$1
-else
-  SESSION='terminal'
-fi
+WINDOW_SESSION='terminal'
+TMUX_DEVELOPER_DIR=${TMUX_DEVELOPER_DIR:-$HOME/Developer}
 
-tmux new-session -s $SESSION -d
+
+### Create Session ###
+echo "creating session (${WINDOW_SESSION})"
+
+tmux new-session -s ${WINDOW_SESSION} -d
 
 
 ### Terminal Window 1 ###
+echo "creating t1 window (${WINDOW_SESSION})"
 
 tmux rename-window t1
 tmux split-window -v
 tmux split-window -h
 
 tmux select-pane -t 0
-tmux send-keys "cd ~; clear"
+tmux send-keys "cd ${TMUX_DEVELOPER_DIR}; clear"
 tmux clear-history
 
 tmux select-pane -t 1
-tmux send-keys "cd ~; clear"
+tmux send-keys "cd ${TMUX_DEVELOPER_DIR}; clear"
 tmux clear-history
 
 tmux select-pane -t 2
-tmux send-keys "cd ~; clear"
+tmux send-keys "cd ${TMUX_DEVELOPER_DIR}; clear"
 tmux clear-history
 
 tmux select-pane -t 0
 
 
 ### Terminal Window 2 ###
+echo "creating t2 window (${WINDOW_SESSION})"
 
 tmux new-window -n t2
 tmux split-window -v
 tmux split-window -h
 
 tmux select-pane -t 0
-tmux send-keys "cd ~; clear"
+tmux send-keys "cd ${TMUX_DEVELOPER_DIR}; clear"
 tmux clear-history
 
 tmux select-pane -t 1
-tmux send-keys "cd ~; clear"
+tmux send-keys "cd ${TMUX_DEVELOPER_DIR}; clear"
 tmux clear-history
 
 tmux select-pane -t 2
-tmux send-keys "cd ~; clear"
+tmux send-keys "cd ${TMUX_DEVELOPER_DIR}; clear"
 tmux clear-history
 
 tmux select-pane -t 0
---
+++ scripts/tmux/tmux_link.zsh
@@ -0,0 +1,8 @@
+TMUX_WINDOWS=('terminal:t1' 'terminal:t2')
+
+
+### Link Windows ###
+
+for w in $TMUX_WINDOWS; do
+  tmux link-window -s "$w"
+done
