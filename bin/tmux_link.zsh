11b7954d5b9c708905f74ac91406bee3bf3fe61b
jonr22 <jonr2219@gmail.com>
2015-11-12 14:40:38 -0500

    Add tmuxinator
    - add default tmuxinator script
    - create tmuxinator scripts for all projects
    - remove old tmux scripts

--- bin/tmux_link.zsh
+++
@@ -1,9 +0,0 @@
-# TMUX_WINDOWS=('terminal:t1' 'terminal:t2' 'weechat:irc')
-TMUX_WINDOWS=('terminal:t1' 'terminal:t2')
-
-
-### Link Windows ###
-
-for w in $TMUX_WINDOWS; do
-  tmux link-window -s "$w"
-done
