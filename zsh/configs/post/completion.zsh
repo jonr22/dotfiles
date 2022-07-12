2885c19a63f5c1a9cb3eff66804d78e3c9f3ac98
jonr22 <jonr2219@gmail.com>
2022-07-12 06:09:36 -0400

    remove asdf from autocomplete

--- zsh/configs/post/completion.zsh
+++ zsh/configs/post/completion.zsh
@@ -1,5 +1,5 @@
 # load our own completion functions
-fpath=($ZSH/completions /usr/local/share/zsh/site-functions /Users/jon/.asdf/completions $fpath)
+fpath=($ZSH/completions /usr/local/share/zsh/site-functions $fpath)
 
 # completion
 autoload -U compinit
