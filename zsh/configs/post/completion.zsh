31a6d84ae3454c8e9c522f06757d7ee316bec903
jonr22 <jonr2219@gmail.com>
2024-02-03 16:16:52 -0500

    fix fzf on mac and update zsh completions

--- zsh/configs/post/completion.zsh
+++ zsh/configs/post/completion.zsh
@@ -1,6 +1,10 @@
 # load our own completion functions
 fpath=($ZSH/completions /usr/local/share/zsh/site-functions $fpath)
 
+if [ -d "/opt/homebrew/share/zsh/site-functions" ]; then
+  fpath=(/opt/homebrew/share/zsh/site-functions $fpath)
+fi
+
 # completion
 autoload -U compinit
 compinit
